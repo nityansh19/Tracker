62a16716a93311f611d0e7c40895d9b10bdff422
Nityansh Rupesh Bahadur <nityansh.bahadur1905@gmail.com>
2026-09-09 21:27:15 +0530

    Add app layout

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'ScoutOP Follower Tracker',
+  description: 'Minimal Instagram follower growth tracker for @scoutop'
+};
+
+export default function RootLayout({ children }) {
+  return <html lang="en"><body>{children}</body></html>;
+}
